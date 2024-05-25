d634623aaa34056436447a68ee5627cf3cc17c01
seven <Seve_tis@proton.me>
2024-05-25 18:37:20 +0800

    complete 2/5 04

--- exercises/04_traits/04_derive/src/lib.rs
+++ exercises/04_traits/04_derive/src/lib.rs
@@ -8,12 +8,13 @@
 // print both sides of the comparison to the terminal.
 // If the compared type doesn't implement `Debug`, it doesn't know how to represent them!
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 struct Ticket {
     title: String,
     description: String,
     status: String,
 }
+
 
 #[cfg(test)]
 mod tests {
